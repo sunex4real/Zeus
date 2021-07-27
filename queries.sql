a616862b3e2172d23e0c92fb9e1a985f0f7e0a7f
Master Wayne <masterwayne@Okories-MacBook-Pro.local>
2021-07-27 08:11:59 +0000

    Modified SQL 3 Answer

--- queries.sql
+++ queries.sql
@@ -17,7 +17,7 @@ FROM
 session (in minutes)? */
 
 WITH
-  transactions AS (
+  DENORM_TABLE AS (
   SELECT
     fullvisitorid,
     visitNumber,
@@ -29,15 +29,69 @@ WITH
     screenName,
     landingScreenName,
     time,
-    ROW_NUMBER() OVER(PARTITION BY fullvisitorid, visitId, date, visitStartTime ORDER BY time ASC) AS rank
+    (
+    SELECT
+      value
+    FROM
+      UNNEST(customDimensions)
+    WHERE
+      INDEX = 11) AS screen,
+    (
+    SELECT
+      value
+    FROM
+      UNNEST(customDimensions)
+    WHERE
+      INDEX = 15) AS locationCountry,
+    (
+    SELECT
+      value
+    FROM
+      UNNEST(customDimensions)
+    WHERE
+      INDEX = 16) AS locationCity,
+    (
+    SELECT
+      value
+    FROM
+      UNNEST(customDimensions)
+    WHERE
+      INDEX = 18) AS locationLon,
+    (
+    SELECT
+      value
+    FROM
+      UNNEST(customDimensions)
+    WHERE
+      INDEX = 19) AS locationLat,
+    (
+    SELECT
+      value
+    FROM
+      UNNEST(customDimensions)
+    WHERE
+      INDEX = 25) AS orderPaymentMethod,
+    (
+    SELECT
+      value
+    FROM
+      UNNEST(customDimensions)
+    WHERE
+      INDEX = 40) AS userLoggedIn
   FROM
     `dhh-analytics-hiringspace.GoogleAnalyticsSample.ga_sessions_export` ga,
-    UNNEST(ga.hit)
+    UNNEST(ga.hit) ),
+  order_confirmation AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER(PARTITION BY fullvisitorid, visitId, visitStartTime ORDER BY time ASC) AS rank
+  FROM
+    DENORM_TABLE
   WHERE
-    eventCategory LIKE '%order_confirmation%')
+    screen = 'order_confirmation' )
 SELECT
-  ROUND(AVG(time / 60000)) as avg_time_mins
+  ROUND(SUM(time / 60000) / COUNT(DISTINCT CONCAT(fullvisitorid, CAST(visitid AS string)))) AS avg_time_mins
 FROM
-  transactions
+  order_confirmation
 WHERE
   rank = 1
