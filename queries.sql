bb472e29770a12c2f908288bbb0cb2283f9ada05
Master Wayne <masterwayne@Okories-MacBook-Pro.local>
2021-07-28 10:41:22 +0000

    Modified SQL Question 4 Answer by Adding the phase two

--- queries.sql
+++ queries.sql
@@ -169,7 +169,7 @@ WITH
       'shop_list',
       'checkout')),
   --- This table filters out visitors that completed the Journey i.e got to the Order Placement Screen.
-  final AS (
+  visitors_reaching_last_journey AS (
   SELECT
     *,
     CONCAT(coalesce(locationLat),',',coalesce(locationLon)) AS curr_cord,
@@ -185,7 +185,7 @@ WITH
     FROM
       journey
     WHERE
-      rank = 3) )
+      rank = 3) ) 
 SELECT
   fullvisitorid,
   visitStartTime,
@@ -207,8 +207,163 @@ END
 END
   address_change_flag
 FROM
-  final
+  visitors_reaching_last_journey 
 ORDER BY
   fullvisitorid,
   visitStartTime,
   rank ASC
+
+
+
+
+/*
+Then, using the BackendDataSample table, see if those customers who changed their
+address ended placing orders and if those orders were delivered successfully, if so, did
+they match their destination.
+*/
+
+WITH
+  -- This Temporal Table Structures the Base table and extract the field values in the custom dimention column
+  DENORM_TABLE AS (
+  SELECT
+    fullvisitorid,
+    visitId,
+    visitStartTime,
+    date,
+    eventAction,
+    time,
+    (
+    SELECT
+      value
+    FROM
+      UNNEST(customDimensions)
+    WHERE
+      INDEX = 11) AS screen,
+    (
+    SELECT
+      value
+    FROM
+      UNNEST(customDimensions)
+    WHERE
+      INDEX = 15) AS locationCountry,
+    (
+    SELECT
+      value
+    FROM
+      UNNEST(customDimensions)
+    WHERE
+      INDEX = 16) AS locationCity,
+    SAFE_CAST((
+      SELECT
+        value
+      FROM
+        UNNEST(customDimensions)
+      WHERE
+        INDEX = 18) AS FLOAT64) AS locationLon,
+    SAFE_CAST((
+      SELECT
+        value
+      FROM
+        UNNEST(customDimensions)
+      WHERE
+        INDEX = 19) AS FLOAT64) AS locationLat
+  FROM
+    `dhh-analytics-hiringspace.GoogleAnalyticsSample.ga_sessions_export` ga,
+    UNNEST(ga.hit) ),
+  -- This Table Creates the Journey and Ranks them in sequencial order e.g Home, Checkout, Order Placement
+  journey AS (
+  SELECT
+    *,
+    DENSE_RANK() OVER(PARTITION BY fullvisitorid, visitId, visitStartTime ORDER BY CASE WHEN screen IN ('home', 'shop_list') THEN '1st Stage'
+        WHEN screen IN ('checkout') THEN '2nd Stage'
+        WHEN screen IN ('order_confirmation') THEN '3rd Stage'
+    END
+      ) AS rank
+  FROM
+    denorm_table
+  WHERE
+    screen IN ('order_confirmation',
+      'home',
+      'shop_list',
+      'checkout')),
+  --- This table filters out visitors that completed the Journey i.e got to the Order Placement Screen.
+  visitors_reaching_last_journey AS (
+  SELECT
+    *,
+    CONCAT(coalesce(locationLat),',',coalesce(locationLon)) AS curr_cord,
+    --This Function extracts the previous cell value in a given row, this would be useful when checking for Change in address
+    LAG(CONCAT(coalesce(locationLat),',',coalesce(locationLon))) OVER(PARTITION BY fullvisitorid, visitId, visitStartTime, rank ORDER BY time ASC) AS prev_cord
+  FROM
+    journey
+    --Filter for only customers that reached the last funnel
+  WHERE
+    CONCAT(coalesce(fullvisitorid),' ',coalesce(visitStartTime)) IN (
+    SELECT
+      CONCAT(coalesce(fullvisitorid),' ',coalesce(visitStartTime))
+    FROM
+      journey
+    WHERE
+      rank = 3) ),
+  change_flag_table AS (
+  SELECT
+    fullvisitorid,
+    visitStartTime,
+    ST_GeogPoint(SAFE_CAST(locationlon AS float64),
+      SAFE_CAST(locationLat AS float64)) AS geo_point,
+    eventAction,
+    CASE
+      WHEN screen IN ('home', 'shop_list') THEN 'Home'
+      WHEN screen IN ('checkout') THEN 'Checkout'
+      WHEN screen IN ('order_confirmation') THEN 'Order Placement'
+  END
+    AS screen,
+    curr_cord,
+    prev_cord,
+    CASE
+      WHEN eventAction IN ('address.submitted', 'address_update.submitted') AND curr_cord != prev_cord THEN 'Changed'
+    ELSE
+    'Not Changed'
+  END
+    address_change_flag,
+    time
+  FROM
+    visitors_reaching_last_journey
+  ORDER BY
+    fullvisitorid,
+    visitStartTime,
+    rank ASC),
+  latest_cord_of_vistors_that_changed_address AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER(PARTITION BY fullvisitorid, visitStartTime ORDER BY time DESC) AS rank
+  FROM
+    change_flag_table
+  WHERE
+    address_change_flag = 'Changed')
+SELECT
+   * except(rank)
+FROM
+  latest_cord_of_vistors_that_changed_address tb
+JOIN (
+  SELECT
+    ST_ASTEXT(geopointCustomer) geopointCustomer,
+    SUM(CASE
+        WHEN ST_DISTANCE(geopointCustomer, geopointDropoff) <= 10 THEN 1
+      ELSE
+      0
+    END
+      ) AS no_of_orders_within_10_mtrs
+  FROM
+    `dhh-analytics-hiringspace.BackendDataSample.transactionalData`
+  WHERE
+    ((geopointCustomer IS NOT NULL
+        AND geopointDropoff IS NOT NULL))
+  GROUP BY
+    geopointCustomer
+  HAVING
+    no_of_orders_within_10_mtrs > 0 ) txn
+ON
+  ST_CONTAINS(ST_GEOGFROMTEXT(txn.geopointCustomer),
+    tb.geo_point )
+WHERE
+  rank = 1
