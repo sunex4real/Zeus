78e28166d74f34748174b55d848dc6b7b92aa39c
Master Wayne <masterwayne@Okories-MacBook-Pro.local>
2021-08-26 23:58:58 +0000

    Added Logic for Data Fetch

--- solution.py
+++ solution.py
@@ -2,9 +2,19 @@ import dask.dataframe as dd
 import pyarrow
 import os
 
+GA_PATH = os.environ["GA_BUCKET_PATH"]
+TXN_PATH = os.environ["TXN_BUCKET_PATH"]
+
 
 def fetch_data_from_gcs(gcs_path):
-    pass
+    """
+    This function takes the GCS bucket path as input
+    to download the parquet files and returns a pandas
+    dataframe
+    """
+    data = dd.read_parquet(gcs_path, engine='pyarrow')
+    data = data.compute()
+    return data
 
 
 def get_session_details(customer_data):
