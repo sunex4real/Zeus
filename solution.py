5c65e7e3424e1792317cce73979a5987c4598665
Master Wayne <masterwayne@Okories-MacBook-Pro.local>
2021-08-27 00:33:29 +0000

    Added flow for getting details of session

--- solution.py
+++ solution.py
@@ -1,5 +1,4 @@
 import dask.dataframe as dd
-import pyarrow
 import os
 
 GA_PATH = os.environ["GA_BUCKET_PATH"]
@@ -11,6 +10,8 @@ def fetch_data_from_gcs(gcs_path):
     This function takes the GCS bucket path as input
     to download the parquet files and returns a pandas
     dataframe
+    :param gcs_path: GCS Bucket Path
+    :return : Pandas Dataframe
     """
     data = dd.read_parquet(gcs_path, engine='pyarrow')
     data = data.compute()
@@ -18,7 +19,31 @@ def fetch_data_from_gcs(gcs_path):
 
 
 def get_session_details(customer_data):
-    pass
+    """
+    This function takes the customer dataframe as input
+    then convert to python dictionary for efficient processing
+    to calculate the coordinates change during the session
+    and also the transactionid if exist
+
+    :param customer_data: Customer Dataframe that contains the
+    actions generated in the session.
+    :return tuple: (Boolean, str) where index 0 is the flag that
+    shows True if there is a change in address and False otherwise
+    while index 1 is for transactionid if exist
+    """
+    events = customer_data.to_dict()[0]
+    lat_lon_cordinates = set()
+    transactionid = set()
+    for event in events:
+        custom_dimensions = event['customDimensions']
+
+        lat_lon_cordinates.update([lat_lon['value'] for lat_lon in custom_dimensions if lat_lon['index'] in (19, 18) and lat_lon['value'] not in ('NA')])
+        transactionid.update([transaction['value'] for transaction in custom_dimensions if transaction['index'] == 36])
+
+    if len(lat_lon_cordinates) > 2:
+        return (True, transactionid.pop())
+    else:
+        return (False, transactionid.pop())
 
 
 def get_transaction_details(transactionid):
